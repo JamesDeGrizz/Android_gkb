23d7ad75a60c3379903aa50b37b30be9a8e833a3
JamesDeGrizz <xoce90@mail.ru>
2020-10-09 00:55:39 +0300

    fix second activity and city label text #2

--- app/src/main/java/com/degrizz/james/android_gkb/helloworld/MainActivityCities.java
+++ app/src/main/java/com/degrizz/james/android_gkb/helloworld/MainActivityCities.java
@@ -2,6 +2,7 @@ package com.degrizz.james.android_gkb.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,22 +16,40 @@ public class MainActivityCities extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_cities);
 
+        Intent intent = new Intent();
+
         Button citiesCloseBtn = findViewById(R.id.buttonCitiesClose);
-        citiesCloseBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                setContentView(R.layout.activity_main);
-            }
+        citiesCloseBtn.setOnClickListener((View v) -> {
+            setResult(RESULT_CANCELED, intent);
+            finish();
         });
 
         ImageView moscowView = findViewById(R.id.imageViewMoscow);
-        moscowView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                TextView cityLabel = findViewById(R.id.textViewChoosedCity);
-                cityLabel.setText("Moscow");
-                setContentView(R.layout.activity_main);
-            }
+        moscowView.setOnClickListener((View v) -> {
+            intent.putExtra("cityLabel", getString(R.string.citiesMoscowLabelText));
+            setResult(RESULT_OK, intent);
+            finish();
+        });
+
+        ImageView petersburgView = findViewById(R.id.imageViewPeterburg);
+        petersburgView.setOnClickListener((View v) -> {
+            intent.putExtra("cityLabel", getString(R.string.citiesPetersburgLabelText));
+            setResult(RESULT_OK, intent);
+            finish();
+        });
+
+        ImageView volgogradView = findViewById(R.id.imageViewVolgograd);
+        volgogradView.setOnClickListener((View v) -> {
+            intent.putExtra("cityLabel", getString(R.string.citiesVolgogradLabelText));
+            setResult(RESULT_OK, intent);
+            finish();
+        });
+
+        ImageView kazanView = findViewById(R.id.imageViewKazan);
+        kazanView.setOnClickListener((View v) -> {
+            intent.putExtra("cityLabel", getString(R.string.citiesKazanLabelText));
+            setResult(RESULT_OK, intent);
+            finish();
         });
     }
 }
