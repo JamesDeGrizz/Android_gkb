4340ec999a888b962241c7d46114561d907f6a28
alexey.korneev <alexey.korneev@tech5-sa.com>
2020-10-22 19:37:02 +0300

    wip #6

--- app/src/main/java/com/degrizz/james/android_gkb/helloworld/MainActivity.java
+++ app/src/main/java/com/degrizz/james/android_gkb/helloworld/MainActivity.java
@@ -5,11 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements Constants {
 
